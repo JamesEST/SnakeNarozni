e8a2fef813ea5ca191816978ed7087b52a3ac320
Vladislav Narozni <farges314@outlook.com>
2020-03-24 11:56:29 +0200

    Добавьте файлы проекта.

---
+++ SnakeNarozni/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeNarozni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80,
+                                  25);
+
+            // Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+            point p = new point(4, 5, '*');
+            p.draw();
+            Console.ReadLine();
+        }
+    }
+}
